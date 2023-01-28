4d2214d64014c8b569f074451939560466ccc604
Lorain <743804605@qq.com>
2023-01-28 11:14:05 +0800

    merge & quick sort

--- index/912-sortArray/main.go
+++ index/912-sortArray/main.go
@@ -1,11 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	nums := []int{5, 2, 3, 1}
 	sorted := sortArrayWithMergeSort(nums)
 	fmt.Println(sorted)
+}
+
+func sortArrayWithQuickSort(nums []int) []int {
+	quickSort(nums, 0, len(nums)-1)
+	return nums
+}
+
+func quickSort(nums []int, left, right int) {
+	// 边界条件，若l > r则非法，若l=r则只有一个元素而无需处理
+	if left >= right {
+		return
+	}
+
+	// 找[right, left]的随机数
+	randIdx := left + rand.Intn(right-left+1)
+	nums[left], nums[randIdx] = nums[randIdx], nums[left] // 交换: 提升接近有序的case性能
+	pivot := nums[left]
+
+	// partition的过程，让a[:idx]<a[0] && a[idx:]>=nums[0]
+	idx := left
+	// all in nums[left+1, idx] <= pivot
+	// all in nums(idx, right] > pivot
+	for i := left + 1; i <= right; i++ { // i从left+1开始，是因为最终要将nums[left]和nums[idx]交换
+		if nums[i] <= pivot { // 大不管，小放前，等号无所谓
+			idx++
+			nums[i], nums[idx] = nums[idx], nums[i]
+		}
+	}
+
+	// partition后，将a[left]放在正确的位置
+	nums[left], nums[idx] = nums[idx], nums[left]
+
+	// 规模变小并分治
+	quickSort(nums, left, idx-1)
+	quickSort(nums, idx+1, right)
 }
 
 func sortArrayWithMergeSort(nums []int) []int {
@@ -19,6 +57,7 @@ func sortArrayWithMergeSort(nums []int) []int {
 		mid := left + ((right - left) >> 1)
 		mergeSort(nums, left, mid)
 		mergeSort(nums, mid+1, right)
+		// merge
 		i, j, k := left, mid+1, 0
 		for i <= mid && j <= right {
 			if nums[i] <= nums[j] {
@@ -47,4 +86,34 @@ func sortArrayWithMergeSort(nums []int) []int {
 	}
 	mergeSort(nums, 0, len(nums)-1)
 	return nums
+}
+
+// prefer this one
+func sortArrayWithMergeSort2(nums []int, left, right int) {
+	if left < right {
+		mid := (left + right) / 2
+		sortArrayWithMergeSort2(nums, left, mid)
+		sortArrayWithMergeSort2(nums, mid+1, right)
+		merge(nums, left, mid, right)
+	}
+}
+
+func merge(nums []int, left, mid, right int) {
+	i, j := left, mid+1
+	var tmp []int
+	for i <= mid && j <= right {
+		if nums[i] < nums[j] {
+			tmp = append(tmp, nums[i])
+			i++
+		} else {
+			tmp = append(tmp, nums[j])
+			j++
+		}
+	}
+	if i <= mid {
+		tmp = append(tmp, nums[i:mid+1]...)
+	} else {
+		tmp = append(tmp, nums[j:right+1]...)
+	}
+	copy(nums[left:right+1], tmp)
 }
