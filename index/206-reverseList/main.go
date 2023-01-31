bb3ef833aa0dc125ac7eb974f9a4cbbf1f3319cb
Lorain <743804605@qq.com>
2023-01-31 22:00:27 +0800

    day13-reverseList

--- index/206-reverseList/main.go
+++ index/206-reverseList/main.go
@@ -13,12 +13,24 @@ func reverseList(head *ListNode) *ListNode {
 	var prev *ListNode
 	curr := head
 	for curr != nil {
+		// 存储前一个节点
 		next := curr.Next
-
+		// 更改当前节点引用
 		curr.Next = prev
+		// 将当前节点设置为下一个节点的前一个节点
 		prev = curr
-
+		// 前进到下一个节点
 		curr = next
 	}
 	return prev
+}
+
+func reverseListWithRecursion(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	newHead := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
 }
