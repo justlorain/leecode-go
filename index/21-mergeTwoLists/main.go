4cfe0b94dc453b00dc8be1e54ac67d763b03b98f
Lorain <743804605@qq.com>
2023-02-02 23:24:58 +0800

    day15-mergeTwoListsWithRecursion

--- index/21-mergeTwoLists/main.go
+++ index/21-mergeTwoLists/main.go
@@ -29,3 +29,39 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	}
 	return dummyHead.Next
 }
+
+func mergeTwoListsWithRecursion(list1, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	} else if list2 == nil {
+		return list1
+	} else if list1.Val < list2.Val {
+		list1.Next = mergeTwoListsWithRecursion(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoListsWithRecursion(list1, list2.Next)
+		return list2
+	}
+}
+
+func mergeTwoLists2(list1, list2 *ListNode) *ListNode {
+	dummyHead := &ListNode{}
+	temp := dummyHead
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			temp.Next = list1
+			list1 = list1.Next
+		} else {
+			temp.Next = list2
+			list2 = list2.Next
+		}
+		temp = temp.Next
+	}
+	if list1 != nil {
+		temp.Next = list1
+	}
+	if list2 != nil {
+		temp.Next = list2
+	}
+	return dummyHead.Next
+}
